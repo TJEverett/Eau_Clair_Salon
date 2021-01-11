5d6bebd96d01a95f4e2ee6161cb571b96372843f
Tristen Everett <spock214@gmail.com>
2021-01-11 11:23:05 -0800

    create basic Index view for each controller holding only links to other controller Index views

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/Clients")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,25 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet("/Stylists")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
