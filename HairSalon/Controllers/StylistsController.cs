53ea95d47a87644c5b926f60d7606bcbcc929cb2
Tristen Everett <spock214@gmail.com>
2021-01-14 11:04:38 -0800

    create the show view to see detials about a specific stylist

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -36,5 +36,12 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/Stylists/{id}")]
+    public ActionResult Show(int id)
+    {
+      Stylist thisStylist = _db.Stylists.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
   }
 }
